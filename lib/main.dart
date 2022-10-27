bd70c8845537a6533399481b1ac73e67bbdb5d44
Julian Kalis <julian@familienzirkus.org>
2022-10-27 09:35:47 +0100

    process of adjusting to dashbaord sign in

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,38 @@ import 'package:flutter/material.dart';
 import 'package:iihf_template/components/toDoList.dart';
 import 'helpers/web3functions.dart';
 import 'package:flutter_web3/flutter_web3.dart';
+import 'package:get_storage/get_storage.dart';
 
-void main() {
+void main() async {
+  await GetStorage.init();
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  String role = "";
+
+  void setRole(value) {
+    setState(() {
+      role = value;
+    });
+  }
+
+  renderBody() {
+    if (role == "") {
+      return const MyHomePage(title: 'Smart Contract ToDo List');
+    } else if (role == "scout") {
+      return Container();
+    } else {
+      return Container();
+    }
+  }
 
   // This widget is the root of your application.
   @override
@@ -17,13 +42,12 @@ class MyApp extends StatelessWidget {
       title: 'IIHF Demo application',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        colorScheme: const ColorScheme.dark().copyWith(
-            background: Colors.grey[900],
-            secondary: Colors.orange[400],
-            primary: Colors.grey[900],
-            tertiary: Colors.green[300]),
+        colorScheme: const ColorScheme.light().copyWith(
+          secondary: Colors.black,
+          primary: Colors.white,
+        ),
       ),
-      home: const MyHomePage(title: 'Smart Contract ToDo List'),
+      home: renderBody(),
     );
   }
 }
@@ -68,31 +92,9 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
-  void submitTask() {
-    if (newTask == "") {
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(
-          content: const Text("Can't add an empty task"),
-          backgroundColor: Colors.red[300],
-        ),
-      );
-      return;
-    }
-
-    Web3FunctionsForWeb().addTask(newTask);
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          widget.title,
-          style: const TextStyle(
-            fontFamily: 'Spartan',
-          ),
-        ),
-      ),
       body: Center(
         child: Wrap(
           crossAxisAlignment: WrapCrossAlignment.center,
@@ -100,174 +102,97 @@ class _MyHomePageState extends State<MyHomePage> {
           spacing: 50,
           children: <Widget>[
             SizedBox(
-              height: 400,
+              height: 600,
               width: 400,
               child: Card(
                 elevation: 50,
                 shadowColor: Colors.black,
                 color: Theme.of(context).colorScheme.primary,
                 child: Padding(
-                  padding: const EdgeInsets.all(15.0),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    mainAxisSize: MainAxisSize.max,
-                    children: <Widget>[
-                      const Text(
-                        "My ToDo's",
-                        style: TextStyle(
-                          fontSize: 30,
-                          fontFamily: 'Spartan',
-                        ),
-                      ),
-                      SizedBox(
-                        width: 250,
-                        height: 250,
-                        child: MyToDos(
-                          listOfTasks: listOfTasks,
-                          key: const Key("mytodos"),
-                        ),
-                      ),
-                      TextButton(
-                        style: ButtonStyle(
-                          side: MaterialStateProperty.all(BorderSide(
-                            color: Theme.of(context).colorScheme.secondary,
-                          )),
-                          overlayColor: MaterialStateProperty.all(
-                            Colors.black,
-                          ),
-                        ),
-                        onPressed: () => {fetchTasks()},
-                        child: const Text("Get my tasks",
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontFamily: 'Spartan',
-                            )),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ),
-            SizedBox(
-              height: 400,
-              width: 400,
-              child: Card(
-                elevation: 50,
-                shadowColor: Colors.black,
-                color: Theme.of(context).colorScheme.primary,
-                child: Padding(
-                  padding: const EdgeInsets.all(15.0),
+                  padding: const EdgeInsets.all(25.0),
                   child: Column(
                     mainAxisSize: MainAxisSize.max,
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: <Widget>[
                       const Text(
-                        "Add Task",
+                        "For Scouts",
                         style: TextStyle(
                           fontSize: 30,
                           fontFamily: 'Spartan',
                         ),
                       ),
-                      TextFormField(
-                        cursorColor: Theme.of(context).colorScheme.secondary,
-                        style: const TextStyle(
-                          color: Colors.white,
-                          fontFamily: 'Spartan',
-                        ),
-                        decoration: InputDecoration(
-                          hintStyle: const TextStyle(
-                            color: Color(0xFFBDBDBD),
-                          ),
-                          hintText: "Enter the task to add",
-                          enabledBorder: const UnderlineInputBorder(
-                            borderSide: BorderSide(
-                              color: Colors.white,
-                            ),
-                          ),
-                          focusedBorder: UnderlineInputBorder(
-                            borderSide: BorderSide(
-                                color: Theme.of(context).colorScheme.secondary),
-                          ),
-                        ),
-                        onChanged: (value) => {addTask(value)},
-                      ),
-                      const SizedBox(
-                        height: 50,
-                      ),
-                      TextButton(
-                        style: ButtonStyle(
-                          side: MaterialStateProperty.all(BorderSide(
-                            color: Theme.of(context).colorScheme.secondary,
-                          )),
-                          overlayColor: MaterialStateProperty.all(
-                            Colors.black,
-                          ),
-                        ),
-                        onPressed: () => {submitTask()},
-                        child: const Text("Add Task",
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontFamily: 'Spartan',
-                            )),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ),
-            SizedBox(
-              height: 400,
-              width: 400,
-              child: Card(
-                elevation: 50,
-                shadowColor: Colors.black,
-                color: Theme.of(context).colorScheme.primary,
-                child: Padding(
-                  padding: const EdgeInsets.all(15.0),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: <Widget>[
                       const Text(
-                        "Wallet connection",
+                        "Some sample text",
                         style: TextStyle(
-                          fontSize: 30,
+                          fontSize: 15,
                           fontFamily: 'Spartan',
                         ),
                       ),
                       TextButton(
                         style: ButtonStyle(
-                          side: MaterialStateProperty.all(BorderSide(
-                            color: Theme.of(context).colorScheme.secondary,
-                          )),
-                          overlayColor: MaterialStateProperty.all(
+                          backgroundColor: MaterialStateProperty.all(
                             Colors.black,
                           ),
                         ),
-                        onPressed: () => {connectWallet()},
-                        child: const Text("Connect Wallet",
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontFamily: 'Spartan',
-                            )),
-                      ),
-                      if (connected == true)
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Icon(
-                              Icons.verified,
-                              color: Theme.of(context).colorScheme.tertiary,
-                            ),
-                            const SizedBox(width: 25),
-                            Text(
-                              "Wallet connected",
+                        onPressed: () => {},
+                        child: const Padding(
+                          padding: EdgeInsets.all(10.0),
+                          child: Text("Sign in",
                               style: TextStyle(
-                                color: Theme.of(context).colorScheme.tertiary,
+                                color: Colors.white,
                                 fontFamily: 'Spartan',
-                              ),
-                            )
-                          ],
-                        )
+                                fontSize: 18,
+                              )),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 600,
+              width: 400,
+              child: Card(
+                elevation: 50,
+                shadowColor: Colors.black,
+                color: Theme.of(context).colorScheme.primary,
+                child: Padding(
+                  padding: const EdgeInsets.all(25.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      const Text(
+                        "For Athletes",
+                        style: TextStyle(
+                          fontSize: 30,
+                          fontFamily: 'Spartan',
+                        ),
+                      ),
+                      const Text(
+                        "Some sample text",
+                        style: TextStyle(
+                          fontSize: 15,
+                          fontFamily: 'Spartan',
+                        ),
+                      ),
+                      TextButton(
+                        style: ButtonStyle(
+                          backgroundColor: MaterialStateProperty.all(
+                            Colors.black,
+                          ),
+                        ),
+                        onPressed: () => {},
+                        child: const Padding(
+                          padding: EdgeInsets.all(10.0),
+                          child: Text("Sign in",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontFamily: 'Spartan',
+                                fontSize: 18,
+                              )),
+                        ),
+                      ),
                     ],
                   ),
                 ),
