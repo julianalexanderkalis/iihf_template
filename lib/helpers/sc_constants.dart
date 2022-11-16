23f9d9f418dd31ef724e7d71f78408abd6f43d1d
Julian Kalis <julian@familienzirkus.org>
2022-11-16 21:50:04 +0100

    commented helper functions and cleaned a little bit

--- lib/helpers/sc_constants.dart
+++ lib/helpers/sc_constants.dart
@@ -1,9 +1,17 @@
+// ignore_for_file: slash_for_doc_comments
+
+// defines the hash address of the Smart Contract on the Blockchain
 const String scAddress = "0xe6d580536FdBAb818bF6b71bFDa905f80A34dcAb";
 
+/**
+ * Defines the ABI (Application Binary Interface) of the callable functions of the Smart Contract.
+ *  Type: List of Strings
+ */
 const scabi = <String>[
-  // for real functions
   'function generateAccessRequest(address[] accountsRequested)',
   'function viewAccessRequests() view returns (tuple[](uint256 id, address requestSender, address[] accountsRequested, bool[] requestsAccepted, uint256 createdAt))',
   'function viewAccessRequestsScout() view returns (tuple[](uint256 id, address requestSender, address[] accountsRequested, bool[] requestsAccepted, uint256 createdAt))',
   'function approveAccessRequest(uint256 id)'
 ];
+
+// NOTE: How to write these Strings can be seen from the compiled Smart Contracts generated ABI file puzzled together
